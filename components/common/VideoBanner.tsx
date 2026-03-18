efe14bb5abee2d59fd5c5f11bd325410611a0e0f
T1mothy Nj4u <93652350+TimothyNjau@users.noreply.github.com>
2026-03-18 12:10:03 +0300

    feat:Change to the Video link to Media page.

--- components/common/VideoBanner.tsx
+++ components/common/VideoBanner.tsx
@@ -3,19 +3,18 @@ import Link from "next/link";
 const VideoBanner = () => {
   return (
     <section className="relative h-64 sm:h-96 md:h-[50vh] lg:h-screen mb-6 md:mb-10 lg:mb-20">
-      <Link href="/Media" className="absolute inset-0 z-0">
+      <Link href={"Media"}>
         <video
-          className="absolute top-0 left-0 w-full h-full object-cover rounded-lg"
-          src="/video/guards_video.mp4"
-          autoPlay
-          muted
-          playsInline
-          loop={true}
-        />
-        <div className="absolute inset-0 bg-black/55"></div>
-      </Link>
+        className="absolute top-0 left-0 w-full h-full object-cover rounded-lg"
+        src="/video/guards_video.mp4"
+        autoPlay
+        muted
+        playsInline
+        loop={true}
+      />
+      <div className="absolute inset-0 bg-black/55"></div>
 
-      <div className="relative z-10 flex flex-col justify-center h-full text-center text-white px-3 sm:px-4">
+      <div className="relative z-2 flex flex-col justify-center h-full text-center text-white px-3 sm:px-4">
         <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-2 sm:mb-4">
           We Secure, God Protects
         </h1>
@@ -25,10 +24,12 @@ const VideoBanner = () => {
       </div>
       <Link
         href={"Quote"}
-        className="absolute bottom-3 right-3 sm:bottom-6 sm:right-6 bg-sec hover:bg-sec/40 px-3 sm:px-4 py-2 rounded-md transition-colors duration-300 text-center text-white text-sm sm:text-base md:text-lg lg:text-xl z-20"
+        className="absolute bottom-3 right-3 sm:bottom-6 sm:right-6 bg-sec hover:bg-sec/40 px-3 sm:px-4 py-2 rounded-md transition-colors duration-300 text-center text-white text-sm sm:text-base md:text-lg lg:text-xl z-10"
       >
         Request A Quote
       </Link>
+      </Link>
+      
     </section>
   );
 };
